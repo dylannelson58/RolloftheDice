278c7ac2a55323da994769a5caeb7b84ed69bf92
dylan <dylannelson58@gmail.com>
2021-02-19 08:31:15 -0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RolloftheDice")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("RolloftheDice")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("91ddea38-d964-46c0-b7c5-d6e6121434cf")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ RolloftheDice.vb
@@ -0,0 +1,40 @@
+﻿Module RolloftheDice
+
+    Sub Main()
+        Dim randomNumbers(10) As Integer
+        Dim header As Integer
+        Dim result As Integer
+
+        For i = 0 To 1000
+            result = GetRandomNumber(5) + GetSecondRandomNumber(5)
+            randomNumbers(result) += 1
+
+            'does not work
+        Next
+
+        header = CInt((77 - 16) / 2)
+        Console.WriteLine(StrDup(header, " ") & "Roll of the Dice" & StrDup(header, " "))
+        Console.WriteLine(StrDup(77, "-"))
+
+        For i = 2 To 12
+            Console.Write(Str(i).PadLeft(6) & "|")
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine(StrDup(77, "-"))
+
+        For i = 0 To UBound(randomNumbers)
+            Console.Write(CStr(randomNumbers(i)).PadLeft(6) & "|")
+        Next
+        Console.Read()
+    End Sub
+    Private Function GetRandomNumber(maxNumber As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))
+    End Function
+
+    Private Function GetSecondRandomNumber(maxNumber As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))
+    End Function
+End Module
